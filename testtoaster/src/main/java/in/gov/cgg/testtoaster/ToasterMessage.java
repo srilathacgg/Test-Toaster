18b4a55cbc7e3ffede50fec27a02e0e559801535
sindhu.k <sindhu.k@cgg.gov.in>
2019-11-13 15:25:16 +0530

    Check Internet Connection

--- testtoaster/src/main/java/in/gov/cgg/testtoaster/ToasterMessage.java
+++ testtoaster/src/main/java/in/gov/cgg/testtoaster/ToasterMessage.java
@@ -1,6 +1,8 @@
 package in.gov.cgg.testtoaster;
 
+import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.widget.Toast;
 
 public class ToasterMessage {
@@ -8,5 +10,21 @@ public class ToasterMessage {
     public static void toasterMethod(Context context, String message) {
         Toast.makeText(context, "Toast here..", Toast.LENGTH_SHORT).show();
 
+    }
+
+    /**
+     * Check whether the internet connection is present or not. <uses-permission
+     * android:name="android.permission.ACCESS_NETWORK_STATE" />
+     */
+    // To check whether network connection is available on device or not
+    public static boolean checkInternetConnection(Activity _activity) {
+        ConnectivityManager conMgr = (ConnectivityManager) _activity
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conMgr.getActiveNetworkInfo() != null
+                && conMgr.getActiveNetworkInfo().isAvailable()
+                && conMgr.getActiveNetworkInfo().isConnected())
+            return true;
+        else
+            return false;
     }
 }
