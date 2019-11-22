fb1808d75c92aa82a0639080e9a6b8279f0db323
sindhu.k <sindhu.k@cgg.gov.in>
2019-11-22 12:54:44 +0530

    test dialog

---
+++ testtoaster/src/main/java/in/gov/cgg/testtoaster/CustomDialogClass.java
@@ -0,0 +1,78 @@
+package in.gov.cgg.testtoaster;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class CustomDialogClass extends Dialog
+{
+
+    public Activity c;
+    public Dialog d;
+    public Button yes, no;
+
+    public static void showExitFromAppAlert(final Activity _activity,
+                                            String title, String alertMsg) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(_activity);
+        alert.setTitle(title);
+        alert.setCancelable(false);
+        alert.setMessage(alertMsg);
+        alert.setView(R.layout.error_dialog);
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                _activity.moveTaskToBack(true);
+                android.os.Process.killProcess(android.os.Process.myPid());
+                System.exit(1);
+
+            }
+        });
+        alert.setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alert.show();
+    }
+
+    public CustomDialogClass(Activity a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+        this.c = a;
+    }
+
+
+   /* used to show error dialog
+    can use R.layout.error_dialog or your custom view
+    as input for parameter view .*/
+
+    public void showCustomAlertDialog(final Activity _activity,
+                                String title, String alertMsg,View view){
+        AlertDialog.Builder alert = new AlertDialog.Builder(_activity);
+        alert.setTitle(title);
+        alert.setCancelable(false);
+        alert.setMessage(alertMsg);
+        alert.setView(view);
+
+        alert.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alert.show();
+
+    }
+
+
+}
