a8169a9b3f7202b1d6e6db3b9177814ba3e12b06
sindhu.k <sindhu.k@cgg.gov.in>
2019-11-26 11:59:35 +0530

    Aadhar and Pan validations commit

---
+++ testtoaster/src/main/java/in/gov/cgg/testtoaster/Validations.java
@@ -0,0 +1,116 @@
+package in.gov.cgg.testtoaster;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class Validations {
+
+
+    public static boolean validateMobileNumber(String mobilenumber) {
+        boolean flag = true;
+        if (mobilenumber != null) {
+            if (!(
+                    (mobilenumber.length() == 10)
+                            && (
+                            (mobilenumber.startsWith("9"))
+                                    || (mobilenumber.startsWith("8"))
+                                    || (mobilenumber.startsWith("7"))
+                                    || (mobilenumber.startsWith("6")))
+            )) {
+                flag = false;
+            }
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validateMobileNumber(EditText mobileEditText) {
+        String mobilenumber = mobileEditText.getText().toString().trim();
+        boolean flag = true;
+        if (mobilenumber != null) {
+            if (!(
+                    (mobilenumber.length() == 10)
+                            && (
+                            (mobilenumber.startsWith("9"))
+                                    || (mobilenumber.startsWith("8"))
+                                    || (mobilenumber.startsWith("7"))
+                                    || (mobilenumber.startsWith("6")))
+            )) {
+                flag = false;
+            }
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validateEditText(EditText editText) {
+        boolean flag = true;
+        if (editText.getText().toString().trim() != null) {
+            if (!((editText.getText().toString().trim().length() > 0))) {
+                flag = false;
+            }
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validateEditText(String string) {
+        boolean flag = true;
+        if (string.trim() != null) {
+            if (!((string.trim().length() > 0))) {
+                flag = false;
+            }
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validateSpinner(Spinner spinner) {
+        boolean flag = true;
+        if (spinner.getSelectedItemPosition() == 0) {
+            flag = false;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validateAadhar(String aadharNo) {
+        boolean flag = true;
+        if ((aadharNo != null)) {
+            if (aadharNo.length() != 12)
+                flag = false;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validateAadhar(EditText aadharNo) {
+        boolean flag = true;
+        if ((aadharNo.getText().toString().trim() != null)) {
+            if (aadharNo.getText().toString().trim().length() != 12)
+                flag = false;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+    public static boolean validatePanNo(String panNo) {
+        boolean flag = true;
+        if ((panNo != null)) {
+            if (panNo.length() != 10 || !panNo.matches("[A-Z]{5}[0-9]{4}[A-Z]{1}"))
+                flag = false;
+        } else {
+            flag = false;
+        }
+        return flag;
+    }
+
+}
